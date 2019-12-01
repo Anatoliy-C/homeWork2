22c9528081f72072bffc7266be93ece76d2cf99a
Anatoliy <atsarkov.13@gmail.com>
2019-12-01 12:15:10 +0300

    calc

--- js/script.js
+++ js/script.js
@@ -214,4 +214,51 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+
+
+    // calc
+
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        resDeys = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum)*4000;
+
+        if(resDeys.value == '') {
+            totalValue.innerHTML=0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    resDeys.addEventListener('change', function() {
+        daysSum = +this.value;
+        total = (daysSum + personsSum)*4000;
+
+        if(persons.value == '') {
+            totalValue.innerHTML=0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if (resDeys.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
+
 });
